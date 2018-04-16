16cfa18158c5629b28fdd50bac80271aceb13641
Ashwin <ayardi@grepruby.com>
2018-04-16 20:45:07 +0530

    Handeled 404 with interceptor

--- src/app/services/app.interceptor.ts
+++ src/app/services/app.interceptor.ts
@@ -12,11 +12,16 @@ export class AppInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    return next.handle(req).do((event: HttpEvent<any>) => {}, (err: any) => {
+    return next.handle(req).do((event: HttpEvent<any>) => { return event; }, (err: any) => {
       if ((err.status >= 500 && err.status < 600) || err.status <= 0) {
         return err.error = {
           err: "There is some error on server, Please try again later",
           err_code: "Server Error"
+        }
+      } else if (err.status == 404) {
+        return err.error = {
+          err: "The page you requested is not found on the server",
+          err_code: "Not Found"
         }
       }
     });
